9f551d0054de4706bd943a79c8dce000c82de009
Lawrence Neal <nealla@lwneal.com>
2018-10-16 12:21:29 -0700

    Add latent space traversal and graph generation

--- denoising_autoencoder.py
+++ denoising_autoencoder.py
@@ -4,47 +4,48 @@ import json
 import numpy as np
 import random
 
+from skimage.draw import polygon
 from tqdm import tqdm
 import imutil
 from logutil import TimeSeries
 
-latent_size = 4
+latent_size = 2
 batch_size = 32
-iters = 10 * 1000
+iters = 50 * 1000
 
 env = None
 prev_states = None
 
 
-class BoxEnv():
-    def __init__(self):
-        # The "true" latent space is four values: x, y, height, width
-        self.x = np.random.randint(15, 25)
-        self.y = np.random.randint(15, 25)
-        self.radius = np.random.randint(5, 8)
-        self.rotation = np.random.uniform(0, 2*np.pi)
-        self.build_state()
+# First, construct a dataset
+def random_example():
+    x = np.random.randint(15, 25)
+    y = np.random.randint(15, 25)
+    radius = np.random.randint(6, 12)
+    rotation = np.random.uniform(0, 2*np.pi)
+    noisy_input = build_box(x, y, radius, rotation)
+    target_output = build_box(x=20, y=20, radius=9, rotation=rotation)
+    return noisy_input, target_output
 
-    def build_state(self):
-        from skimage.draw import polygon
-        self.state = np.zeros((40,40,3))
-        def polar_to_euc(r, theta):
-            return (self.y + r * np.sin(theta), self.x + r * np.cos(theta))
-        points = [polar_to_euc(self.radius, self.rotation + t) for t in [
-            np.pi/4, 3*np.pi/4, 5*np.pi/4, 7*np.pi/4]]
-        r, c = zip(*points)
-        rr, cc = polygon(r, c)
-        self.state[rr, cc] = 1.
+
+def build_box(x, y, radius, rotation):
+    state = np.zeros((1, 40, 40))
+    def polar_to_euc(r, theta):
+        return (y + r * np.sin(theta), x + r * np.cos(theta))
+    points = [polar_to_euc(radius, rotation + t) for t in [
+        np.pi/4, 3*np.pi/4, 5*np.pi/4, 7*np.pi/4]]
+    r, c = zip(*points)
+    rr, cc = polygon(r, c)
+    state[:, rr, cc] = 1.
+    return state
 
 
 def build_dataset(size=50000):
     dataset = []
-    for i in tqdm(range(size)):
-        env = BoxEnv()
-        example = np.moveaxis(env.state, -1, 0)
-        dataset.append(example)
+    for i in tqdm(range(size), desc='Building Dataset...'):
+        x, y = random_example()
+        dataset.append((x, y))
     return dataset  # list of examples
-
 
 dataset = build_dataset()
 
@@ -52,10 +53,10 @@ dataset = build_dataset()
 def get_batch(size=32):
     idx = np.random.randint(len(dataset) - size)
     examples = dataset[idx:idx + size]
-    return examples
+    return zip(*examples)
 
 
-# ok now we build a neural network
+# Now, let's build an autoencoder in Pytorch
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -65,56 +66,31 @@ class Encoder(nn.Module):
     def __init__(self, latent_size):
         super().__init__()
         self.latent_size = latent_size
-        # 1x40x40
-        self.conv1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        # 32x40x40
-        self.conv2 = nn.Conv2d(32, 64, 4, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        # 64x20x20
-        self.conv3 = nn.Conv2d(64, 64, 4, stride=2, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        # 64x10x10
-        self.conv4 = nn.Conv2d(64, 64, 4, stride=2, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
-        # 64x5x5
-        self.conv5 = nn.Conv2d(64, 64, 3, stride=1, padding=0)
-        self.bn5 = nn.BatchNorm2d(64)
-        # 64x3x3
-        self.fc1 = nn.Linear(64*3*3, 512)
-        self.bn_fc1 = nn.BatchNorm1d(512)
-        self.fc2 = nn.Linear(512, latent_size)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 3, kernel_size=4, stride=2, padding=1)
+
+        self.fc1 = nn.Linear(20*20*3, 512)
+        self.bn2 = nn.BatchNorm1d(512)
+        self.fc2 = nn.Linear(512, 512)
+        self.bn3 = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, latent_size)
         self.cuda()
 
     def forward(self, x):
-        # Input: batch x 32 x 32
-        #x = x.unsqueeze(1)
-        # batch x 1 x 32 x 32
         x = self.conv1(x)
         x = self.bn1(x)
         x = F.leaky_relu(x, 0.2)
-
         x = self.conv2(x)
+        x = x.view(-1, 20*20*3)
+
+        x = self.fc1(x)
         x = self.bn2(x)
         x = F.leaky_relu(x, 0.2)
-
-        x = self.conv3(x)
+        x = self.fc2(x)
         x = self.bn3(x)
         x = F.leaky_relu(x, 0.2)
-
-        x = self.conv4(x)
-        x = self.bn4(x)
-        x = F.leaky_relu(x, 0.2)
-
-        x = self.conv5(x)
-        x = self.bn5(x)
-        x = F.leaky_relu(x, 0.2)
-
-        x = x.view(-1, 64*3*3)
-        x = self.fc1(x)
-        x = self.bn_fc1(x)
-
-        z = self.fc2(x)
+        z = self.fc3(x)
         return z
 
 
@@ -124,22 +100,21 @@ class Decoder(nn.Module):
         self.latent_size = latent_size
         self.fc1 = nn.Linear(latent_size, 512)
         self.bn0 = nn.BatchNorm1d(512)
-
         self.fc2 = nn.Linear(512, 256)
 
-        self.deconv1 = nn.ConvTranspose2d(256, 128, 5, stride=1)
+        self.deconv1 = nn.ConvTranspose2d(256, 128, kernel_size=5, stride=1)
         self.bn1 = nn.BatchNorm2d(128)
         # 128 x 5 x 5
-        self.deconv2 = nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
         # 64 x 10 x 10
-        self.deconv3 = nn.ConvTranspose2d(64, 64, 4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
         self.bn3 = nn.BatchNorm2d(64)
         # 64 x 20 x 20
-        self.deconv4 = nn.ConvTranspose2d(64, 64, 4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
         self.bn4 = nn.BatchNorm2d(64)
         # 64 x 40 x 40
-        self.deconv5 = nn.ConvTranspose2d(64, 3, 3, stride=1, padding=1)
+        self.deconv5 = nn.ConvTranspose2d(64, 1, kernel_size=3, stride=1, padding=1)
         self.cuda()
 
     def forward(self, z):
@@ -173,14 +148,17 @@ class Decoder(nn.Module):
 
 
 def main():
-    # ok now, can the network learn the task?
+    # Create a 40x40 monochrome image autoencoder
     encoder = Encoder(latent_size)
     decoder = Decoder(latent_size)
-    opt_encoder = optim.Adam(encoder.parameters(), lr=0.0001)
-    opt_decoder = optim.Adam(decoder.parameters(), lr=0.0001)
+    opt_encoder = optim.Adam(encoder.parameters())
+    opt_decoder = optim.Adam(decoder.parameters())
 
+    demo_batch, demo_targets = get_batch()
+    vid = imutil.VideoLoop('autoencoder_reconstruction')
     ts = TimeSeries('Training', iters)
 
+    # Train the network on the denoising autoencoder task
     for i in range(iters):
         encoder.train()
         decoder.train()
@@ -188,13 +166,14 @@ def main():
         opt_encoder.zero_grad()
         opt_decoder.zero_grad()
 
-        examples = get_batch()
+        batch_input, batch_target = get_batch()
 
-        x = torch.Tensor(examples).cuda()
+        x = torch.Tensor(batch_input).cuda()
+        y = torch.Tensor(batch_target).cuda()
+
         z = encoder(x)
         x_hat = decoder(z)
-
-        loss = F.binary_cross_entropy(x_hat, x)
+        loss = F.binary_cross_entropy(x_hat, y)
         ts.collect('Reconstruction loss', loss)
 
         loss.backward()
@@ -204,12 +183,31 @@ def main():
         encoder.eval()
         decoder.eval()
 
-        if i % 100 == 0:
+        if i % 25 == 0:
             filename = 'iter_{:06}_reconstruction.jpg'.format(i)
-            img = torch.cat([x[:2], x_hat[:2]])
+            x = torch.Tensor(demo_batch).cuda()
+            z = encoder(x)
+            x_hat = decoder(z)
+            img = torch.cat([x[:4], x_hat[:4]])
             caption = 'iter {}: orig. vs reconstruction'.format(i)
-            imutil.show(img, filename=filename, resize_to=(256,256), img_padding=10, caption=caption, font_size=8)
+            imutil.show(img, filename=filename, resize_to=(256,512), img_padding=10, caption=caption, font_size=8)
+            vid.write_frame(img, resize_to=(256,512), img_padding=10, caption=caption, font_size=12)
         ts.print_every(2)
+
+    # Now examine the representation that the network has learned
+    EVAL_FRAMES = 240
+    z = torch.Tensor((1, latent_size)).cuda()
+    ts_eval = TimeSeries('Evaluation', EVAL_FRAMES)
+    vid_eval = imutil.VideoLoop('latent_space_traversal')
+    for i in range(EVAL_FRAMES):
+        theta = 2*np.pi*(i / EVAL_FRAMES)
+        box = build_box(20, 20, 10, theta)
+        z = encoder(torch.Tensor(box).unsqueeze(0).cuda())[0]
+        ts_eval.collect('Latent Dim 1', z[0])
+        ts_eval.collect('Latent Dim 2', z[1])
+        caption = "Theta={:.2f} Z_0={:.3f} Z_1={:.3f}".format(theta, z[0], z[1])
+        pixels = imutil.show(box, resize_to=(512,512), caption=caption, font_size=12, return_pixels=True)
+        vid_eval.write_frame(pixels)
     print(ts)
 
 
